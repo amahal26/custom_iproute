b1c7c29bbd8c1a2202279d942a74aa36752c644a
amahal26 <harunori.amano.4z@stu.hosei.ac.jp>
2022-01-14 14:29:42 +0900

    update

--- lib/exec.c
+++ lib/exec.c
@@ -11,25 +11,8 @@ int cmd_exec(const char *cmd, char **argv, bool do_fork,
 	     int (*setup)(void *), void *arg)
 {
 	fflush(stdout);
-	pid_t pid;
-
-	pid = fork();
-	if (pid < 0) {
-		perror("fork");
-		exit(1);
-	}
-
-	if (pid != 0) {
-		int status=0;
-		pid=wait(&status);
-		exit(1);
-	}
-	else {
-		if (setup && setup(arg))
-		return -1;
-		if (execvp(cmd, argv)  < 0)
+	
+	if (execvp(cmd, argv)  < 0)
 			fprintf(stderr, "exec of \"%s\" failed: %s\n",cmd, strerror(errno));
-		exit (EXIT_SUCCESS);
-	}
 	_exit(1);
 }
